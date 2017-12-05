34025f13e860c4c1e09451b58d0920df4aedf2d3
RAnderso0325 <reillyjanderson@gmail.com>
2017-12-05 14:11:44 -0800

    ryan gosling format is mostly complete

---
+++ js/app.js
@@ -0,0 +1,7 @@
+$(function(){
+    $("#tabs").tabs();
+});
+$(function(){
+    $(".accordion").accordion();
+});
+$('.parallax-window').parallax({imageSrc: 'http://del.h-cdn.co/assets/17/07/980x490/landscape-1487365300-delish-ryan-gosling-getty-pascal.jpg'});
